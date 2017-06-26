6d035b44a13a9344822d2d8adacefb290135abb5
Adam Abram <aabram.hs@gmail.com>
2017-06-26 11:57:19 -0400

    Finished 1st R script

--- scripts.R
+++ scripts.R
@@ -1,8 +1,17 @@
-#Compares the means of columns within in "range" of two given datasets. A positive value means dset2>dset1
+#runs the wilcox test against two datasets for columns in "range"
 #range should be a vector of at least two numbers (e.g. 2:7 or c(1,4,5,6,7,99))
 #Dsets must have columns line up (e.g. they both contain final grade data on column x) to give a meaningful answer
 #values are named after dset1 columns
+#returns a named matrix with w and p values
 testChange <- function(dset1,dset2,range){
-	diffMean = colMeans(dset2[,range]) - colMeans(dset1[,range])
-	return (diffMean)
+	temp = matrix (nrow = 2, ncol = length(range))
+	i = 1
+	dimnames(temp) = list(c("W", "p"), colnames(dset1[range]))
+	for(col in range){
+		output = wilcox.test(dset1[,col], dset2[,col])
+		temp[1,i] = output$statistic
+		temp[2,i] = output$p.value
+		i= 1 + i
+	}
+	return (temp)
 }
